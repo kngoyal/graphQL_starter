8822d7893c337be27ffd01be7fc2441a25026fc6
Goyal <goyal kamal@tor-c02y21j7j.bcg.com>
2019-02-17 14:22:19 -0500

    connected to mongoDB.Atlas instance

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql'); // not a package but a convention for express-graphql
 const schema = require('./schema/schema');
+const config = require('./config/config');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connect to mongoDB.Atlas database
+const {mongoDB: {username,password,cluster,dbname}} = config;
+mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}/${dbname}?retryWrites=true`, {useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+  console.log('connected to mongoDB');
+})
 
 app.use("/graphql", graphqlHTTP({
   schema,
